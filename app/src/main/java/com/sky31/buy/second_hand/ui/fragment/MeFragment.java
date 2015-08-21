8d6586fe89e73d0cce77dc217f6c9bb94cd6a7d0
CasparGX <630248976@qq.com>
2015-08-21 18:09:34 +0800

    edit,添加登录框

--- app/src/main/java/com/sky31/buy/second_hand/ui/fragment/MeFragment.java
+++ app/src/main/java/com/sky31/buy/second_hand/ui/fragment/MeFragment.java
@@ -2,6 +2,9 @@ package com.sky31.buy.second_hand.ui.fragment;
 
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -9,7 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -21,6 +26,7 @@ import com.sky31.buy.second_hand.util.HttpUtil;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -30,23 +36,41 @@ public class MeFragment extends Fragment implements View.OnClickListener {
 
     private String TAG = this.getTag();
 
+    /*登录状态*/
+    private boolean isLogin;
+
+    /*控件*/
     private TableRow tr01;
     private TableRow tr02;
+    private TextView tvNickname;
+    private TextView tvEmail;
+
+
+    private AlertDialog.Builder builderLogin;
+    private EditText mEtPassWord;
+    private EditText mEtUserName;
 
     private RequestParams params = new RequestParams();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        builderLogin = new AlertDialog.Builder(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, container, false);
+
+        /*TableRow*/
         tr01 = (TableRow) view.findViewById(R.id.tr_user);
         tr02 = (TableRow) view.findViewById(R.id.tr_02);
         tr01.setOnClickListener(this);
         tr02.setOnClickListener(this);
+
+        /*TableRow Content*/
+        tvEmail = (TextView) view.findViewById(R.id.tv_email);
+        tvNickname = (TextView) view.findViewById(R.id.tv_nickname);
 
         return view;
     }
@@ -67,9 +91,20 @@ public class MeFragment extends Fragment implements View.OnClickListener {
             super.onSuccess(statusCode, headers, response);
             Log.i(TAG, "-----------------开始: 接受到JSONObject数据-----------------");
             Log.i(TAG, response + "");
-            System.out.println(headers + "");
-            System.out.print(headers);
+            try {
+                if (response.get("result").equals("error")) {
+                    Toast.makeText(getActivity(),"登录失败:"+response.get("msg")+"",Toast.LENGTH_SHORT).show();
+                } else if (response.get("result").equals("success")) {
+                    //Toast.makeText(getActivity(),"登录成功",Toast.LENGTH_SHORT).show();
+                    response = (JSONObject) response.get("msg");
+                    loginSuccess(response);
+                } else{
+                    Toast.makeText(getActivity(),"服务器无响应",Toast.LENGTH_SHORT).show();
+                }
 
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             Log.i(TAG, "-----------------结束: 接受到JSONObject数据-----------------");
         }
 
@@ -87,15 +122,56 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         }
     };
 
+    /*登录成功*/
+    private void loginSuccess(JSONObject response) {
+        try {
+            tvEmail.setText(response.get("email")+"");
+            tvNickname.setText(response.get("nickname")+"");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*点击事件*/
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tr_user:
-                params.add(Constants.Keys.KEY_USERNAME,"630248976@qq.com");
-                params.add(Constants.Keys.KEY_PASSWORD,"Caspar1995.");
-                HttpUtil.post(Constants.Apis.API_USER_LOGIN_POST
-                        , params
-                        , mJsonHttpResponseHandler);
+
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.dialog_signin, null);
+                builderLogin
+                        .setTitle(R.string.login)
+                        .setView(dialogView)
+                        .setPositiveButton("确定",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        mEtUserName = (EditText) dialogView.findViewById(R.id.username);
+                                        mEtPassWord = (EditText) dialogView.findViewById(R.id.password);
+
+                                        //登录
+//                                        params.add(Constants.Keys.KEY_USERNAME,"630248976@qq.com");
+                                        //params.add(Constants.Keys.KEY_PASSWORD,"Caspar1995.");
+                                        //删除已有参数,防止参数过多
+                                        if (params.has(Constants.Keys.KEY_USERNAME))
+                                            params.remove(Constants.Keys.KEY_USERNAME);
+                                        if (params.has(Constants.Keys.KEY_PASSWORD))
+                                            params.remove(Constants.Keys.KEY_PASSWORD);
+                                        params.add(Constants.Keys.KEY_USERNAME,mEtUserName.getText().toString());
+                                        params.add(Constants.Keys.KEY_PASSWORD,mEtPassWord.getText().toString());
+                                        HttpUtil.post(Constants.Apis.API_USER_LOGIN_POST
+                                                , params
+                                                , mJsonHttpResponseHandler);
+
+                                        Log.i(TAG, String.valueOf(mEtUserName.getText().toString().equals("630248976@qq.com")));
+                                        Log.i(TAG, params+"");
+
+                                    }
+                                }).setNegativeButton("取消", null).create()
+                        .show();
                 break;
             case R.id.tr_02:
                 File file = new File("/mnt/sdcard/","home.jpg");
@@ -122,22 +198,5 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    AsyncHttpResponseHandler mAsyncHttpResponseHandler = new AsyncHttpResponseHandler() {
 
-
-        @Override
-        public void onSuccess(int i, Header[] headers, byte[] bytes) {
-
-            Log.i(TAG, "-----------------AsyncHttp  onSuccess-----------------");
-            for (int j = 0; j < bytes.length; j++ ) {
-                Log.i(TAG, String.valueOf(bytes[j]));
-            }
-        }
-
-        @Override
-        public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-            Log.i(TAG, "-----------------AsyncHttp  onFailure-----------------");
-            Log.i(TAG, String.valueOf(i));
-        }
-    };
 }
