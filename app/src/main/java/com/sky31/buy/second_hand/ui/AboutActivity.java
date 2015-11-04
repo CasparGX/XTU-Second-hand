731887c6eb59f8023ed4e832fefca93e0ef34987
CasparGX <630248976@qq.com>
2015-11-04 17:02:46 +0800

    edit

--- app/src/main/java/com/sky31/buy/second_hand/ui/AboutActivity.java
+++ app/src/main/java/com/sky31/buy/second_hand/ui/AboutActivity.java
@@ -1,12 +1,20 @@
 package com.sky31.buy.second_hand.ui;
 
+import android.app.AlertDialog;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sky31.buy.second_hand.R;
 import com.sky31.buy.second_hand.context.BuyApp;
@@ -59,6 +67,11 @@ public class AboutActivity extends SwipeBackActivity implements View.OnClickList
         Uri uri ;
         Intent it ;
         switch (v.getId()) {
+            /* call us */
+            case R.id.tr_call_me:
+                showDialog();
+                break;
+
             /* develop*/
             case R.id.tr_develop:
                 uri = Uri.parse("http://github.com/CasparGX");
@@ -75,4 +88,26 @@ public class AboutActivity extends SwipeBackActivity implements View.OnClickList
         }
 
     }
+
+    /*显示dialog*/
+    public void showDialog() {
+        LayoutInflater inflater = this.getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("联系我们")
+                .setMessage("请联系我们的官方QQ：1643787575")
+                .setPositiveButton("复制号码",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+
+                                ClipboardManager cmb = (ClipboardManager)AboutActivity.this.getSystemService(Context.CLIPBOARD_SERVICE);
+                                cmb.setText("1643787575");
+                                Toast.makeText(AboutActivity.this, "已复制QQ号，可直接粘贴", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setNegativeButton("确定", null).create()
+                .show();
+    }
+
 }
