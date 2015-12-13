170219cac77ff7093f091cfc2263adaca19c80db
CasparGX <630248976@qq.com>
2015-12-13 20:45:19 +0800

    edit

--- app/src/main/java/com/sky31/buy/second_hand/ui/AboutActivity.java
+++ app/src/main/java/com/sky31/buy/second_hand/ui/AboutActivity.java
@@ -72,7 +72,7 @@ public class AboutActivity extends BaseSwipeBackActivity implements View.OnClick
         //tvTitle = (TextView) findViewById(R.id.textView);
         //tvTitle.setText("关于");
         tvVersion = (TextView) findViewById(R.id.tv_version);
-        tvVersion.setText("" + BuyApp.getVersionName(this));
+        tvVersion.setText("版本：" + BuyApp.getVersionName(this));
         iv_back_btn = (ImageView) findViewById(R.id.iv_back_btn);
         iv_back_btn.setOnClickListener(this);
         trCallMe = (TableRow) findViewById(R.id.tr_call_me);
