dd002834e3cc68dc53776841e4dbdefca58171a2
CasparGX <630248976@qq.com>
2015-12-13 18:17:54 +0800

    edit

--- app/src/main/java/com/sky31/buy/second_hand/MainActivity.java
+++ app/src/main/java/com/sky31/buy/second_hand/MainActivity.java
@@ -30,8 +30,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-
         setContentView(R.layout.activity_main);
 
         if (BuyApp.getInstance().isOpen()) {
