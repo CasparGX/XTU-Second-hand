14cfb50003e0bc7dd96812e982ccfb48543eb7f5
CasparGX <630248976@qq.com>
2015-12-15 00:23:23 +0800

    edit

--- app/src/main/java/com/sky31/buy/second_hand/MainActivity.java
+++ app/src/main/java/com/sky31/buy/second_hand/MainActivity.java
@@ -4,16 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
 import com.sky31.buy.second_hand.context.BuyApp;
 import com.sky31.buy.second_hand.model.GoodsData;
 import com.sky31.buy.second_hand.ui.HomeActivity;
 
-import org.apache.http.Header;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -47,42 +43,5 @@ public class MainActivity extends Activity {
                 MainActivity.this.finish();    // 结束启动动画界面
             }
         }, delayTime);    //启动动画
-
-        /*
-        GoodsApi goodsApi = new GoodsApi();
-        RequestParams params = new RequestParams(); // 绑定参数
-        params.put(Constants.Keys.KEY_LIMITID, 0);
-        HttpUtil.get(Constants.Apis.API_GOODS_LIST_GET, params, mJsonHttpResponseHandler);*/
-
     }
-
-    JsonHttpResponseHandler mJsonHttpResponseHandler = new JsonHttpResponseHandler() {
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-            super.onSuccess(statusCode, headers, response);
-            Log.i("GoodsApi: ", "onSuccess ");
-            Log.i("GoodsApi", response + "");
-            mGoodsArray = response;
-            app.addGoodsData(mGoodsArray);
-            Log.i("MainAty : ", mGoodsData.get(0).imgUrl + "");
-        }
-
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-            super.onSuccess(statusCode, headers, response);
-            //if return JSONObject, it's have no data
-            Log.i("GoodsApi", "onSuccess ");
-            Log.i("GoodsApi", response + "");
-        }
-
-        @Override
-        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-            super.onFailure(statusCode, headers, responseString, throwable);
-            Log.e("GoodsApi", " onFailure" + responseString.toString());
-        }
-
-        public void onFinish() {
-            Log.i("GoodsApi", "onFinish");
-        }
-    };
 }
